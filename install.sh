41a9b5ee78f372ef36ea808c2dcaf400ae6845b2
Francesco <fr.ilario@gmail.com>
2022-09-11 10:40:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,21 @@ echo "Installing valved into /usr/local/valved"
 sudo install "bin/valved" "/usr/local/valved"
 
 echo "Starting valved.service"
-sudo systemctl start "deploy/linux/valved.service"
+sudo install "deploy/linux/valved.service" "/usr/lib/systemd/system/valved.service"
+sudo systemctl daemon-reload
+sudo systemctl enable --now "valved.service"
+
+echo "Building 'skeduler' image"
+docker build \
+    -f deploy/docker/skeduler/Dockerfile \
+    -t "rosina/skeduler:latest" \
+    .
+
+echo "Building 'rosina' image"
+docker build \
+    -f deploy/docker/rosina/Dockerfile \
+    -t "rosina/rosina:latest" \
+    .
 
 echo "Appling manifests"
-kubectl apply -f "manifests"
-
+kubectl apply -f "manifests/skeduler.yaml"
